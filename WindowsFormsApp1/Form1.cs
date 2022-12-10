31582533615287deda14a331009477fc62687d39
ANDBAN2223 <andban2@alu.edu.gva.es>
2022-12-10 13:51:34 +0100

    Mensaje boton primer comit usuario 2

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -19,7 +19,8 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("el numero 1");
+            string texto = "el numero 1";
+            MessageBox.Show(texto);
         }
     }
 }
